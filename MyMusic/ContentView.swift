eb26b142ea0f1c242383330869fc3747a05c2cbf
tommy <tmtmtoa@gmail.com>
2023-06-18 17:03:26 +0900

    シンバルとギターのロジック

--- MyMusic/ContentView.swift
+++ MyMusic/ContentView.swift
@@ -3,14 +3,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 音を鳴らすためのSoundPlayerクラスのインスタンス生成
+    let soundPlayer = SoundPlayer()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            // 背景画像を指定する
+            Image("background")
+            // リサイズする
+                .resizable()
+            // 画面いっぱいになるようにセーフエリア外まで表示されるように指定
+                .ignoresSafeArea()
+            // アスペクト比（縦横比）を維持して短辺基準に収まるようにする
+                .scaledToFill()
+            
+            // 水平にレイアウト
+            HStack {
+                // シンバルボタン
+                Button {
+                    soundPlayer.cymbalPlay()
+                } label: {
+                    Image("cymbal")
+                }
+                
+                // ギターボタン
+                Button {
+                    soundPlayer.guitarPlay()
+                } label: {
+                    Image("guitar")
+                }
+            }
         }
-        .padding()
     }
 }
 
